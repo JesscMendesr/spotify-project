2c5d3ab28dd8469f907f420d405b1c1e8282dd4f
Jess 😺 <jessicamend17@gmail.com>
2026-03-04 00:57:45 -0300

    ajuste

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+const redirect = sessionStorage.getItem('redirect');
+sessionStorage.removeItem('redirect');
 
+if (redirect && redirect !== location.href) {
+  history.replaceState(null, '', redirect);
+}
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
