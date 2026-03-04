bd5ec8277d78d9174ceb437d0f080c207dc12d85
Jess 😺 <jessicamend17@gmail.com>
2026-03-04 00:39:43 -0300

    correção rota redirect

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -10,7 +10,7 @@ import { LoadingService } from "./loading/loading.service";
 @Injectable({ providedIn: 'root' })
 export class AuthService {
     private clientId = environment.client_id;
-    private redirectUrl = 'https://unctuous-synaptically-emeline.ngrok-free.dev/callback';
+    private redirectUrl = 'https://jesscmendesr.github.io/spotify-project/callback';
 
     private authorizationEndpoint = "https://accounts.spotify.com/authorize";
 
